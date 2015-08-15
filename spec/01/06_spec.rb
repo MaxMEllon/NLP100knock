b143207585702eaa7c7888fe2f0ba224f9d6ac50
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-16 00:40:41 +0900

    [add] Q06 test case

--- spec/01/06_spec.rb
+++ spec/01/06_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'Q06' do
+describe 'Q06-1' do
   before do
     x = 'paraparaparadise'.char_ngram(2)
     y = 'paragraph'.char_ngram(2)
@@ -9,4 +9,15 @@ describe 'Q06' do
   it { expect(@prog_06.union).to eq %w(pa ar ra ap ad di is se ag gr ph) }
   it { expect(@prog_06.product).to eq %w(pa ar ra ap) }
   it { expect(@prog_06.diff).to eq %w(ad di is se) }
+end
+
+describe 'Q06-2' do
+  before do
+    x = 'para para paradise'.word_ngram(2)
+    y = 'para graph'.word_ngram(2)
+    @prog_06 = Prog06.new(x, y)
+  end
+  it { expect(@prog_06.union).to eq ["parapara", "paraparadise", "paragraph"] }
+  it { expect(@prog_06.product).to eq [] }
+  it { expect(@prog_06.diff).to eq ["parapara", "paraparadise"] }
 end
